7ef70bb1ed1dbfbc94e4db205431d98e837e5322
CostaACDC <kavdeev1970@gmail.com>
2024-06-05 18:42:22 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+# Press the green button in the gutter to run the script.
+
+print(len(str('базаприколов')))
+print(5+7, 5-7)
+print((8+6+9)/3)
+print('понедельник'', ''вторник')
+print((((3*5)+(3*7))**3)/2)
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
