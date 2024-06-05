1754e01c50cc2ed083a525e85eaae524f2bf39d2
CostaACDC <kavdeev1970@gmail.com>
2024-06-05 20:08:31 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,10 +4,27 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 # Press the green button in the gutter to run the script.
-
-print(len(str('базаприколов')))
-print(5+7, 5-7)
-print((8+6+9)/3)
-print('понедельник'', ''вторник')
-print((((3*5)+(3*7))**3)/2)
+a = 'базаприколов'
+print(len(str(a)))
+first = 5
+second = 7
+print(first + second, first - second)
+summa = 12
+diff = -2
+print (summa)
+print (diff)
+first = 8
+second = 6
+third = 9
+print((first + second + third)/3)
+mean = 7.666666666666667
+print (mean)
+first_string = 'вторник'
+second_string = 'понедельник'
+print(second_string,(', '),first_string)
+a = 3
+b = 5
+c = 7
+f = (a*b)+(a*c)
+print((f**3)/2)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
